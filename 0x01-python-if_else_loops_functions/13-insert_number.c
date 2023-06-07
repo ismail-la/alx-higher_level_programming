3e697d12cd01fe68cec7b0d84086ef6bf3ad0606
ismail-la <lahbariismail@gmail.com>
2023-06-06 19:51:30 -0700

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -13,24 +13,20 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node = *head, *new;
-	nnew = malloc(sizeof(listint_t));
-	if (nnew == NULL)
-		return (NULL);
-	nnew->n = number;
-
-	if (node == NULL || node->n >= number)
-	{
-		nnew->next = node;
-		*head = nnew;
-		return (nnew);
-	}
-
-	while (node && node->next && node->next->n < number)
-		node = node->next;
-
-	nnew->next = node->next;
-	node->next = nnew;
-
-	return (nnew);
+listint_t *node = *head, *new_node;
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = number;
+if (node == NULL || node->n >= number)
+{
+new_node->next = node;
+*head = new_node;
+return (new_node);
+}
+while (node && node->next && node->next->n < number)
+node = node->next;
+new_node->next = node->next;
+node->next = new_node;
+return (new_node);
 }
