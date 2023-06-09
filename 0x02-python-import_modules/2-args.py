be1e891c10e08c0c210b8c91dd770994fb59008e
ismail-la <lahbariismail@gmail.com>
2023-06-08 19:21:06 -0700

    0x02. Python - import & modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    arg_count = len(sys.argv) - 1
+    if  arg_count  == 0:
+        print("{} arguments.".format(arg_count))
+    elif arg_count  == 1:
+        print("{} argument:".format(arg_count))
+    else:
+        print("{} arguments:".format(arg_count))
+
+    if arg_count >= 1:
+        arg_count = 0
+        for arg in sys.argv:
+            if arg_count != 0:
+                print("{}: {}".format(arg_count, arg))
+            arg_count += 1
