5207081509c1882dd39c77fde6ee2391e3addd15
ismail-la <lahbariismail@gmail.com>
2023-06-08 19:32:25 -0700

    0x02. Python - import & modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,9 +3,9 @@
 if __name__ == "__main__":
     from sys import argv
     Total_arg = 0
-    Total_arg = len(argv)
+    num_arg = len(argv)
 
-    for x in range(1, Total_arg):
+    for x in range(1, num_arg):
         Total_arg += int(argv[x])
 
     print(Total_arg)
