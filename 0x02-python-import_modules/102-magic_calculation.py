57a3bb9b0ae425bf14ba87e9a87e12ea71a3fb20
ismail-la <lahbariismail@gmail.com>
2023-06-08 19:50:25 -0700

    0x02. Python - import & modules

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    from magic_calculation_102 import subb, add
+    if a < b:
+        s = add(a, b)
+        for x in range(4, 6):
+            s = add(s, x)
+        return (s)
+
+    else:
+        return(sub(a, b))
