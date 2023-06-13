047d990fc6684b344846c42793a6ad34c2d81c29
ismail-la <lahbariismail@gmail.com>
2023-06-12 19:57:48 -0700

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -18,6 +18,8 @@ void print_python_list_info(PyObject *p)
 	printf("[*] Size of the Python List = %li\n", size);
 	printf("[*] Allocated = %li\n", Listobj->allocated);
 	while (i < size)
-		printf("Element %i: %s\n", i, Py_TYPE(Listobj->ob_item[i])->tp_name);
-		i++;
+	{
+	printf("Element %i: %s\n", i, Py_TYPE(Listobj->ob_item[i])->tp_name);
+	i++;
+	}
 }
