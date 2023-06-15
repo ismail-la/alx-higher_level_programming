491f2b52e53617546ae445b6b8778b6d93714da6
ismail-la <lahbariismail@gmail.com>
2023-06-14 19:34:13 -0700

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -54,14 +54,15 @@ void print_python_list(PyObject *p)
 
 	size = ((PyVarObject *)(p))->ob_size;
 	list = (PyListObject *)p;
+	long int allocated = list->allocated;
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
 	printf("[*] Allocated = %ld\n", list->allocated);
 
 	for (i = 0; i < size; i++)
-		obj = ((PyListObject *)p)->ob_item[i];
-		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
+		obj = PyList_GetItem(p, i)
+		printf("Element %ld: %s\n", i, Py_TYPE(obj)->tp_name);
 		if (PyBytes_Check(obj))
 			print_python_bytes(obj);
 }
