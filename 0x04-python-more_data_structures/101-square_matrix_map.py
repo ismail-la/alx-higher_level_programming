b8f72ea3393b6a9c99740b8dd6234b2753638fee
ismail-la <lahbariismail@gmail.com>
2023-06-14 18:49:15 -0700

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_map(matrix=[]):
-    return (list(map((lambda r: list(map((lambda i: i * i), r))), matrix)))
+    return list(map((lambda r: list(map((lambda i: i * i), r))), matrix))
