d2908a3b41c747be569895d5d465f7707800de1a
ismail-la <lahbariismail@gmail.com>
2023-06-14 19:55:45 -0700

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -53,7 +53,7 @@ void print_python_list(PyObject *p)
 {
 	long int size, i;
 	PyListObject *list;
-	PyObject *obj;
+	PyObject *object;
 
 	size = ((PyVarObject *)(p))->ob_size;
 	list = (PyListObject *)p;
@@ -64,9 +64,9 @@ void print_python_list(PyObject *p)
 
 	for (i = 0; i < size; i++)
 	{
-		obj = ((PyListObject *)p)->ob_item[i];
+		object = ((PyListObject *)p)->ob_item[i];
 		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
-		if (PyBytes_Check(obj))
-			print_python_bytes(obj);
+		if (PyBytes_Check(object))
+			print_python_bytes(object);
 	}
 }
