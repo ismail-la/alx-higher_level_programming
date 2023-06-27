e4b6b6e489b82a9cb9268efa9ac4beab1ad93b1f
ismail-la <lahbariismail@gmail.com>
2023-06-26 18:15:59 -0700

    0x05. Python - Exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+def safe_print_list(my_list=[], x=0):
+    """Print x elememts of a list.
+    """
+    sum = 0
+    for j in range(x):
+        try:
+            print(f"{my_list[j]}", end="")
+            sum += 1
+        except IndexError:
+            break
+    print()
+    return(sum)
