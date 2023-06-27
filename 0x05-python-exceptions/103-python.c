e0fb7fef042fe9af2557ededf3edbd906bcb63d4
ismail-la <lahbariismail@gmail.com>
2023-06-26 20:11:11 -0700

    0x05. Python - Exceptions

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,6 +1,4 @@
-
 #include <Python.h>
-#include <stdio.h>
 
 void print_python_list(PyObject *p);
 void print_python_float(PyObject *p);
--- 0x05-python-exceptions/Python.h
+++
@@ -1,8 +0,0 @@
-#ifndef Python_H
-#define Python_H
-
-void print_python_list(PyObject *p);
-void print_python_bytes(PyObject *p);
-void print_python_float(PyObject *p);
-
-#endif /* Python */
