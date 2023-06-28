2324c15e8a8ccc342e94e09a0ec4c39e845efcd6
ismail-la <lahbariismail@gmail.com>
2023-06-27 19:06:01 -0700

    0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a Class Square"""
+
+
+class Square:
+    """create a square"""
+
+    def __init__(self, SIZE_square):
+        """representation of the Square"""
+        self.__size = SIZE_square
