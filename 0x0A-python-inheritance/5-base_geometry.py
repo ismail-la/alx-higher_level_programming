4d64165fd828b30fd8c5b6428cad896287a09285
ismail-la <lahbariismail@gmail.com>
2023-07-12 18:22:03 -0700

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-"""Defines an empty class BaseGeometry -
-checks if object is an instance of a class that
-inherited from the specified class or not
-"""
+"""An empty class"""
 
 
-def inherits_from(obj, a_class):
-    """Represent base geometry -
-    Returns true if object is an instance of a class that inherited
-    (directly or indirectly) from the specified class; otherwise False
-    """
+class BaseGeometry:
+    """represent an empty class"""
 
-    return (issubclass(type(obj), a_class) and type(obj) != a_class)
+    pass
