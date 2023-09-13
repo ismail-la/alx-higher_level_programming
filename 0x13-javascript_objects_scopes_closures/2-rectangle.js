71094201077d4756071d048742f80da962aaef91
ismail-la <lahbariismail@gmail.com>
2023-09-12 19:26:55 -0700

    0x13. JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -6,7 +6,8 @@
  *   Initialize the instance attribute width with the value of w
  *   Initialize the instance attribute height with the value of h
  *   If w or h is equal to 0 or not a positive integer, create an empty object
- *   Create an instance method called print() that prints the rectangle using the character X
+ *   Create an instance method called print()
+ *    that prints the rectangle using the character X
  */
 
 class Rectangle {
