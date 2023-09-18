72cc0d993fcfe19df033f360739fd5dda1f539d2
ismail-la <lahbariismail@gmail.com>
2023-09-17 18:30:52 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This script prints the first State object from the database hbtn_0e_6_usa"""
+"""Script that prints the first State object from the database hbtn_0e_6_usa"""
 
 from sys import argv
 from model_state import State, Base
