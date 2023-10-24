f5767785c63ef22f804c7b976d4ff6e7bbde5120
ismail-la <lahbariismail@gmail.com>
2023-10-24 23:02:25 +0100

    0x14. JavaScript - Web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-/* Script that prints the title of a Star Wars movie where the episode number matches a given integer.
-The first argument is the movie ID
-You must use the Star wars API with the endpoint https://swapi-api.alx-tools.com/api/films/:id
-You must use the module request
+/* Script that prints the title of a Star Wars movie where the episode number
+matches a given integer.
+-The first argument is the movie ID.
+-You must use the Star wars API with the endpoint.
+ https://swapi-api.alx-tools.com/api/films/:id.
+-You must use the module request.
 */
 
 const request = require('request');
