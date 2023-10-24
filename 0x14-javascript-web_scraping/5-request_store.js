d86c858ce495828bbe03d3237d141db0ff6c1b52
ismail-la <lahbariismail@gmail.com>
2023-10-24 23:08:46 +0100

    0x14. JavaScript - Web scraping

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+/*Script that gets the contents of a webpage and stores it in a file.
+The first argument is the URL to request
+The second argument the file path to store the body response
+The file must be UTF-8 encoded
+You must use the module request
+*/
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const File = process.argv[3];
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(File, body, 'utf8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
