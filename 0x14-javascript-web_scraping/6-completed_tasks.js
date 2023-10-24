6c804929fffd4e292c051017778be6b2001a4c84
ismail-la <lahbariismail@gmail.com>
2023-10-24 23:12:50 +0100

    0x14. JavaScript - Web scraping

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+/* script that computes the number of tasks completed by user id.
+The first argument is the API URL: https://jsonplaceholder.typicode.com/todos
+Only print users with completed task
+You must use the module request
+*/
+// Import modules
+const request = require('request');
+
+// Get command arguments
+const url = process.argv[2];
+
+request.get(url, { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const User_tasksCompleted = {};
+
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!User_tasksCompleted[todo.userId]) {
+        User_tasksCompleted[todo.userId] = 1;
+      } else {
+        User_tasksCompleted[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(User_tasksCompleted);
+});
