92c00ddb6752daaa78f10e02d52cd8dc7cac7bd4
ismail-la <lahbariismail@gmail.com>
2023-10-25 02:05:04 +0100

    update

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-/* Script that prints all characters of a Star Wars movie:
-The first argument is the Movie ID - example: 3 = “Return of the Jedi”
-Display one character name by line
-You must use the Star wars API
-You must use the module request
-*/
+//  script that prints all characters of a Star Wars movie
 
 const request = require('request');
 const movie_Id = process.argv[2];
