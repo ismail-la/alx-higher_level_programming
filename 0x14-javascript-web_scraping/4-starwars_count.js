0c19592cb31e91bc060880e8b4c9171ad7825453
ismail-la <lahbariismail@gmail.com>
2023-10-25 01:50:30 +0100

    update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const url = process.argv[2];
 const characterId = '18';
-let Count = 0;
+let count = 0;
 request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
