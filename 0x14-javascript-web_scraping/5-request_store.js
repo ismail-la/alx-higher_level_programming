a3562d614242dc1f38e0aaa32334035b207f3212
ismail-la <lahbariismail@gmail.com>
2023-10-25 01:42:45 +0100

    update

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,12 +3,12 @@
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const filePath = process.argv[3];
+const File_Path = process.argv[3];
 request(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    fs.writeFile(filePath, body, 'utf8', (error) => {
+    fs.writeFile(File_Path, body, 'utf8', (error) => {
       if (error) {
         console.log(error);
       }
