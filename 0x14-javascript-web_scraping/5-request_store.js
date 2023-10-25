198efcf14b18ea9a2d9cb29904e8e034cf54b41e
ismail-la <lahbariismail@gmail.com>
2023-10-25 01:39:59 +0100

    update

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 /*Script that gets the contents of a webpage and stores it in a file.
-The first argument is the URL to request
-The second argument the file path to store the body response
-The file must be UTF-8 encoded
-You must use the module request
-*/
+ *The first argument is the URL to request
+ *The second argument the file path to store the body response
+ *The file must be UTF-8 encoded
+ *You must use the module request
+ */
 
 const request = require('request');
 const fs = require('fs');
