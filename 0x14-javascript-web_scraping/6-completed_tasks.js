c29edd1c2520d84735d868c11fb4ecc877a97a4c
ismail-la <lahbariismail@gmail.com>
2023-10-25 01:59:01 +0100

    update

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//script that computes the number of tasks completed by user id.
+//script that computes the number of tasks completed by user id
 const request = require('request');
 const url = process.argv[2];
 
