bb4e9b77bda334bb72bbf0146ce62b4872116847
ismail-la <lahbariismail@gmail.com>
2023-10-25 02:00:12 +0100

    update

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 // script that computes the number of tasks completed by user id
+
 const request = require('request');
 const url = process.argv[2];
-
 request.get(url, { json: true }, (error, response, body) => {
   if (error) {
     console.log(error);
     return;
   }
-
   const tasksCompleted = {};
   body.forEach((todo) => {
     if (todo.completed) {
